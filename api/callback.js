68acd2058d935d464ac166445cf66ee590abe7ef
eyzul-store <along11fizul@gmail.com>
2026-08-13 02:02:49 +0800

    Delete api/callback.js

--- api/callback.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    return res.status(200).json({ status: 'success' });
-  } else {
-    return res.status(200).send('Webhook endpoint is live!');
-  }
-}
